6c06d60c10645f618b7f65b3b082ea69ea56e1df
RakeshPK <rakesgpk97@gmail.com>
2023-04-19 11:42:45 +0530

    final app

---
+++ src/moviesData.js
@@ -0,0 +1,64 @@
+    export const movies = 
+    [
+
+    {
+        title: "Ironman",
+        plot: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+        price: 399,
+        rating: 8.9,
+        poster: "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_FMjpg_UX1000_.jpg",
+        stars: 4.5,
+        fav: false,
+        isInCart: false
+    },
+    {
+        title: "The Avengers",
+        plot: "Plot of the movie is related to super natural powers",
+        price: 499,
+        rating: 8.0,
+        poster: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+        stars: 4,
+        fav: false,
+        isInCart: false
+    },
+    {
+        title: "Captain America",
+        plot: "Steve Rogers teams up with Natasha Romanoff aka Black Widow, to battle a powerful yet shadowy enemy in present-day Washington, D.C.",
+        price: 299,
+        rating: 8.5,
+        poster: "https://m.media-amazon.com/images/M/MV5BMTYzOTc2NzU3N15BMl5BanBnXkFtZTcwNjY3MDE3NQ@@._V1_.jpg",
+        stars: 4.5,
+        fav: false,
+        isInCart: false
+    },
+    {
+        title: "Ant Man",
+        plot: "Cat burglar Scott Lang must embraceshis Tia Ue tkeand help his mentor, Dr. Hank Pym, plan and pull iS cEeist that will save the world",
+        price: 199,
+        rating: 8.0,
+        poster: "https://m.media-amazon.com/images/M/MV5BMjM2NTQ5Mzc2M15BMl5BanBnXkFtZTgwNTcxMDI2NTE@._V1_FMjpg_UX1000_.jpg",
+        stars: 3.5,
+        fav: false,
+        isInCart: false
+    },
+    {
+        title: "Spider Man - Far from Home",
+        plot: "Plot of the movie is related to super natural powers",
+        price: 199,
+        rating: 8.0,
+        poster: "https://upload.wikimedia.org/wikipedia/en/b/bd/Spider-Man_Far_From_Home_poster.jpg",
+        stars: 0,
+        fav: false,
+        isInCart: false
+    },
+    {
+        title: "Thor-Love And Thunder",
+        plot: "Plot of the movie is related to super natural powers",
+        price: 199,
+        rating: 8.0,
+        poster: "https://m.media-amazon.com/images/M/MV5BYmMxZWRiMTgtZjM0Ny00NDQxLWIxYWQtZDdlNDNkOTEzYTdlXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+        stars: 0,
+        fav:false,
+        isInCart:false
+        }
+]
